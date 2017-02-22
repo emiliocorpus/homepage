4178698f13776e123ac02b364823becaa1ec8a6f
emiliocorpus <emiliocorpus@gmail.com>
2017-02-22 00:28:03 -0800

    basic skeleton

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
