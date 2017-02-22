3eca0dc2fdd0deb0d40c0843db230a33fc676625
emiliocorpus <emiliocorpus@gmail.com>
2017-02-22 13:08:01 -0800

    adds project model

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170222210715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "pic_url"
+    t.string   "web_url"
+    t.string   "github_url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
---
+++ spec/factories/projects.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :project do
+    title "MyString"
+    description "MyText"
+    pic_url "MyString"
+    web_url "MyString"
+    github_url "MyString"
+  end
+end
